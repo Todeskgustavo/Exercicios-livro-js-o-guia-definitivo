44f56482b8309a0537dcc42a3fa4ab0121046f8a
Gustavo Todeschini <gutodeschinii@outlook.com>
2024-01-30 20:52:17 -0300

    Exemplo de caixa modal movendo para outro site

---
+++ Exemplo002/exemplo.js
@@ -0,0 +1,15 @@
+// var resposta = prompt("Digite algo:");
+
+// var confirmacao = confirm("Você tem certeza?");
+
+function moveon() {
+    //Exibe uma caixa de diálogo modal para fazer uma pergunta ao usuário
+    var answer = confirm("Ready to move on?");
+    // Se ele clicou no botão "OK", faz o navegador carregar uma nova página
+    if (answer) window.location = "http://google.com";
+}
+//Executa a função definida acima por 1 minuto (60.000 milisegundos) a partir de agora.
+setTimeout(moveon, 60000);
+
+moveon();
+
