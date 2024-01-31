293f922a52350fdcc889a6f4ed00f50cb250f429
Gustavo Todeschini <gutodeschinii@outlook.com>
2024-01-30 21:00:18 -0300

    Mais exemplos de caixa de diálogo modal

---
+++ Exemplo003/exemplo.js
@@ -0,0 +1,14 @@
+// Exemplo de alerta
+alert("Esta é uma mensagem de alerta!");
+
+//Exemplo de prompt 
+var nome = prompt("Digite seu nome:");
+console.log("Olá, " + nome);
+
+//Exemplo de confirm
+var confirmacao = confirm("Você deseja continuar?");
+if (confirmacao) {
+    console.log("Ação confirmada!");
+} else {
+    console.log("Ação cancelada.");
+}
